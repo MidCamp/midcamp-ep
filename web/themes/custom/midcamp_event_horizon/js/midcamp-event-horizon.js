44dcfce027ce7fdaa0b096b3afafa4573ae620eb
Avi Schwab <avi@imagexmedia.com>
2025-12-10 14:51:53 -0600

    Add sub theme and some assets, but not enabling it yet

---
+++ web/themes/custom/midcamp_event_horizon/js/midcamp-event-horizon.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * midcamp_event_horizon behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  // Drupal.behaviors.midcampEventHorizon = {
+  //   attach (context, settings) {
+  //
+  //     console.log('It works!');
+  //
+  //   }
+  // };
+
+} (Drupal));
